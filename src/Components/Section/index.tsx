de51727e901c41e3ce9c5631fff4f91ef50fb548
lucasalvess96 <lucasalves.57@hotmail.com>
2021-04-14 18:37:37 -0300

    atualizado 14/04/2021

--- src/Components/Section/index.tsx
+++ src/Components/Section/index.tsx
@@ -1,3 +1,4 @@
+import { FaArrowAltCircleDown, FaLayerGroup, FaAudioDescription } from 'react-icons/fa'
 import './style.css'
 
 export default function PageSection (){
@@ -11,52 +12,52 @@ export default function PageSection (){
 
             <div className="diferenciais">
                 <div className="diferenciais-single">
-                    <i className="fas fa-arrow-circle-down"></i>
+                    <i> <FaArrowAltCircleDown /> </i>
                     <h2>Lorem ipsum</h2>
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti tempora minus cupiditate maiores tempore odio necessitatibus distinctio facilis voluptates ab commodi soluta deserunt ad, architecto perferendis! Dignissimos expedita nihil quisquam?</p>
                 </div>
 
                 <div className="diferenciais-single">
-                    <i className="fas fa-layer-group"></i>
+                    <i> <FaLayerGroup /> </i>
                     <h2>Lorem ipsum</h2>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas at minima corrupti, architecto aliquam optio pariatur voluptatum impedit quasi, dolor esse, labore necessitatibus? Eum ipsa laborum fugit ratione consectetur asperiores.</p>
                 </div>
 
                 <div className="diferenciais-single">
-                    <i className="fas fa-audio-description"></i>
+                    <i> <FaArrowAltCircleDown /> </i>
                     <h2>Lorem ipsum</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit minima fugit veritatis harum unde illum adipisci quidem maiores. Nobis eum minima veniam dolorum reiciendis magnam est, dolor sint incidunt omnis!</p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas at minima corrupti, architecto aliquam optio pariatur voluptatum impedit quasi, dolor esse, labore necessitatibus? Eum ipsa laborum fugit ratione consectetur asperiores.</p>
                 </div>
 
                 <div className="diferenciais-single">
-                    <i className="fas fa-audio-description"></i>
+                    <i> <FaAudioDescription /> </i>
                     <h2>Lorem ipsum</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit minima fugit veritatis harum unde illum adipisci quidem maiores. Nobis eum minima veniam dolorum reiciendis magnam est, dolor sint incidunt omnis!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit minima harum unde illum adipisci quidem maiores. Nobis eum minima veniam dolorum reiciendis magnam est, dolor sint incidunt omnis!</p>
                 </div>
 
             </div>
 
             <div className="diferenciais">
                 <div className="diferenciais-single">
-                    <i className="fas fa-arrow-circle-down"></i>
+                    <i> <FaArrowAltCircleDown /> </i>
                     <h2>Lorem ipsum</h2>
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti tempora minus cupiditate maiores tempore odio necessitatibus distinctio facilis voluptates ab commodi soluta deserunt ad, architecto perferendis! Dignissimos expedita nihil quisquam?</p>
                 </div>
 
                 <div className="diferenciais-single">
-                    <i className="fas fa-layer-group"></i>
+                    <i> <FaLayerGroup /> </i>
                     <h2>Lorem ipsum</h2>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas at minima corrupti, architecto aliquam optio pariatur voluptatum impedit quasi, dolor esse, labore necessitatibus? Eum ipsa laborum fugit ratione consectetur asperiores.</p>
                 </div>
 
                 <div className="diferenciais-single">
-                    <i className="fas fa-audio-description"></i>
+                    <i> <FaAudioDescription /> </i>
                     <h2>Lorem ipsum</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit minima fugit veritatis harum unde illum adipisci quidem maiores. Nobis eum minima veniam dolorum reiciendis magnam est, dolor sint incidunt omnis!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit minima harum unde illum adipisci quidem maiores. Nobis eum minima veniam dolorum reiciendis magnam est, dolor sint incidunt omnis!</p>
                 </div>
 
                 <div className="diferenciais-single">
-                    <i className="fas fa-arrow-circle-down"></i>
+                    <i> <FaArrowAltCircleDown /> </i>
                     <h2>Lorem ipsum</h2>
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti tempora minus cupiditate maiores tempore odio necessitatibus distinctio facilis voluptates ab commodi soluta deserunt ad, architecto perferendis! Dignissimos expedita nihil quisquam?</p>
                 </div>
--- src/Pages/About/index.tsx
+++ src/Pages/About/index.tsx
@@ -1,13 +1,72 @@
 import MenuComponentent from "../../Components/Menu"
+import { Link } from 'react-router-dom'
+import {FiArrowLeft} from 'react-icons/fi'
 
 import './style.css'
 function ComponentAbout(){
     return(
         <>
             <MenuComponentent />
-            <div className="flex-item">
-                <h1>Bem-vindo</h1>
-                <h2>Page-About</h2>
+            <div id="about-aside">
+                <aside>
+                    <header>
+                        <h2>Lucas Alves</h2>
+                        <p>
+                            Buscando primeiro estágio ou emprego na area de
+                            Desenvolvedor WEB
+                        </p>
+                    </header>
+
+                    <footer>
+                        <strong>Web-Developer</strong>
+                        <span>na pratica</span>
+                    </footer>
+                </aside>
+
+                <main>
+                    <article>
+                        <img src="https://images.unsplash.com/photo-1543357480-c60d40007a3f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZnJlZWRvbXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt=""/>
+                    </article>
+                    <div className="dados">
+                        <h2>Lucas Eduardo Chagas Alves</h2>
+                        <div className="separacaoInf"></div>
+                        <h3>Dados Pessoais</h3>
+                        <span>Estado Civil: Solteiro</span>
+                        <span>Nacionalidade: Brasileiro</span>
+                        <span>Naturalidade: Paraense</span>
+                        <span>Email:Lucasalves.57@hotmail.com</span>
+                    </div>
+                    <div className="separacaoInf"></div>
+
+                    <div className="infoescolar">
+                        <h3>Formação Escolar</h3>
+                        <span>Ensino Médio Técnico em Secretariado (Completo)</span>
+                        <span>Cursando Ensino Superior, atualmente no 5º período
+                        Em Sistema de Informação - Estácio - FAP</span>
+                    </div>
+                    <div className="separacaoInf"></div>
+
+                    <div className="conhecimento">
+                        <h3>conhecimento</h3>
+                        <span>Básico HTML, CSS, JavaScript, ReactJS,</span>
+                        <span>Básico GitHub</span>
+                        <span>Comandos básicos de SQLServer</span>
+                    </div>
+                    <div className="separacaoInf"></div>
+
+                    <div className="git">
+                        <h3>GitHub</h3>
+                        <Link to=" https://github.com/lucasalvess96?tab=repositories"> https://github.com/lucasalvess96?tab=repositories</Link>
+                        <div className="separacaoInf"></div>
+                    </div>
+
+                    <div className="linkedin">
+                    <h3>linkedin</h3>
+                        <Link to="/https://www.linkedin.com/feed/"> https://www.linkedin.com/feed/</Link>
+                        <div className="separacaoInf"></div>
+                    </div>
+                </main>
+                <Link to="/" className="link-homeback"> <FiArrowLeft size={32} color="#FFF"/> </Link>
             </div>
         </>
     )
